3345c825d56c43615ff740c78543199cb5f910c9
Walid Amine <walidamine272@gmail.com>
2025-11-10 20:36:35 +0100

    chore: pin eslint@8 and ignore lint in CI

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'ddragon.leagueoflegends.com' },
+      { protocol: 'https', hostname: 'raw.communitydragon.org' }
+    ]
+  }
+};
+module.exports = nextConfig;
